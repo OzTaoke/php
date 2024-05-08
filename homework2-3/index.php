b93ab3d010f52f92772539903c8d0438d4daa093
Alexandra Onufrieva <drakshen123@mail.ru>
2024-05-08 18:41:42 +0600

    2 неделя, правки домашнего задания№4

--- homework2-3/index.php
+++ homework2-3/index.php
@@ -1,6 +1,6 @@
 <?php
 include 'orders.php';
 
-$order1 = new orderBasicRate(100,100,'GPS');
-$order2 = new orderHourlyRate(100,100, 'Driver');
-$order3 = new orderStudentRate(100,100);
+orderBasicRate(100,100,'GPS');
+orderHourlyRate(50,50, 'Driver');
+orderStudentRate(150,150);
